152784a305d1a0a5989942f283b872ab62b857f5
Anand Stephan <anandstephan98@gmail.com>
2024-03-06 12:38:13 +0530

    feat:compelete the second project

---
+++ slot-machine/SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView2.swift
+//  slot-machine
+//
+//  Created by Anand Stephan on 06/03/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Anand Stephan on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
